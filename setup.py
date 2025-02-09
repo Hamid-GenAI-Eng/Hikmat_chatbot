4b372929c0d43f392ed54e61b72187c7edeb86f8
Hamid-GenAI-Eng <hamidsaif214@gmail.com>
2025-02-09 21:53:12 +0500

    Updated files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Hikmat Chatbot ",
+    name="Hikmat Chatbot",
     version='0.0.0',
-    auther="Hamid Saifullah",
-    auther_email="Hamidsaif214@gmail.com",
+    author="Hamid Saifullah",
+    author_email="Hamidsaif214@gmail.com",
     packages= find_packages(),
     install_requires=[]
 )
